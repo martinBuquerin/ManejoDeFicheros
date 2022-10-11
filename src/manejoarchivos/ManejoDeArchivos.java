1e512dd93d18a6b5e2f770a1d298645959387d09
martinBuquerin <martin.buquerin.elgue@gmail.com>
2022-10-11 10:08:02 +0200

    Initial commit

---
+++ src/manejoarchivos/ManejoDeArchivos.java
@@ -0,0 +1,16 @@
+/*
+ Nuestra clase ManejoDeArchivos va a contener los siguientes métodos
+-Crear archivo
+-Guardar información
+-Eliminar información de un archivo
+-Eliminar un archivo
+ */
+package manejoarchivos;
+
+/**
+ *
+ * @author Martín
+ */
+public class ManejoDeArchivos {
+    
+}
